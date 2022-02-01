8514819968492d1550db1d64af98e1f738e6060e
okamoto1129 <94105406+okamoto1129@users.noreply.github.com>
2022-02-01 21:15:22 +0900

    python 2章　章末問題
    python 2章　章末問題　回答

--- python/test02.py
+++ python/test02.py
@@ -1,5 +1,7 @@
 from ast import Index
+from msilib.schema import Error
 from multiprocessing.sharedctypes import Value
+from operator import index
 from tkinter import E
 
 
@@ -14,13 +16,24 @@ while True:
         # 問①：if文を用いて、以下の画像のように処理されるように記述しましょう。
         ##ここに書く
         if number == 1:
+            number = int('hoge')
+        elif number == 2:
             number = "hoge"
+            number[8]
+        elif number == 3:
+            print("例外を発生させませんでした")
+            continue
     except ValueError as e:
-        print(e)
+        print("Value Error")
         print(e.args)
+    except IndexError as e:
+        print("Index Error")
+        print(e.args)
+        
     # 問②：else節を用いて、以下の画像のように処理されるように記述しましょう。
     ##ここに書く
     else:
+        print("修了させます")
         break
 print('↓')
 print('無限ループを終了します')
